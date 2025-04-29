e0c825d293e7ccfd3d14b162a90410bc8c1c55a6
Caio Dev <caioinnocencio@gmail.com>
2025-04-29 00:36:29 -0300

    Criando serviço para buscar dados de PIB total e per capita na API do IBGE.

--- desafioconectar/src/services/ibgeService.tsx
+++ desafioconectar/src/services/ibgeService.tsx
@@ -6,4 +6,18 @@ const api = axios.create({
 
 export async function buscarDadosPIB() {
 
+    try {
+        const [pibTotal] = await Promise.all([
+          api.get("/5938/periodos/all/variaveis/37?localidades=N1[all]"),
+
+        ]);
+    
+        return {
+          pibTotal: pibTotal.data,
+        };
+      } catch (error) {
+        console.error("Erro ao buscar dados do IBGE:", error);
+        throw error;
+      }
+
 }
