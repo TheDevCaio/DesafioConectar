142e4904e63f03db3fccbcc1dcdd2947821bc794
Caio Dev <caioinnocencio@gmail.com>
2025-04-30 00:58:29 -0300

    Migrando o endpoint consumido na constante buscarPopulacaoBrasil, para a do worldbank, pois a outra estava fora do ar, mas essa funciona.

--- desafioconectar/src/components/Grafico/index.tsx
+++ desafioconectar/src/components/Grafico/index.tsx
@@ -10,9 +10,10 @@ import {
   ResponsiveContainer,
 } from 'recharts';
 import { GraphWrapper, ChartContainer } from './styled';
-import { buscarDadosPIB, DadosPIB } from '../../services/ibgeservice';
-import { buscarTaxaCambio } from '../../services/taxaCambio';
 
+import { buscarTaxaCambio } from '../../services/taxaCambio';
+import buscarPopulacaoBrasil from '../../services/buscarPopulacaoBrasil';
+import { DadosPIB, buscarDadosPIB } from '../../services/ibgeservice';
 
 const Grafico: React.FC = () => {
   const [dados, setDados] = useState<DadosPIB[]>([]);
@@ -21,11 +22,25 @@ const Grafico: React.FC = () => {
     const fetchData = async () => {
       const dadosPIB = await buscarDadosPIB();
       const taxaCambio = await buscarTaxaCambio();
+      const populacao = await buscarPopulacaoBrasil();
 
-      const dadosEmDolar = dadosPIB.map(dado => ({
-        ...dado,
-        pibTotal: dado.pibTotal / taxaCambio, 
-      }));
+      if (populacao === 0) {
+        console.error('População inválida. Não foi possível calcular o PIB per capita.');
+        return; 
+      }
+
+      const dadosEmDolar = dadosPIB.map(dado => {
+        const pibEmDolar = dado.pibTotal / taxaCambio;
+
+
+        const pibPerCapitaEmDolar = pibEmDolar / populacao;
+
+        return {
+          ...dado,
+          pibTotal: pibEmDolar, 
+          pibPerCapita: pibPerCapitaEmDolar, 
+        };
+      });
 
       setDados(dadosEmDolar);
     };
@@ -35,7 +50,7 @@ const Grafico: React.FC = () => {
 
   return (
     <GraphWrapper>
-      <h1>Evolução do PIB (em Dólar)</h1>
+      <h1>Evolução do PIB (em Dólar) e PIB per Capita (em Dólar)</h1>
       <ChartContainer>
         <ResponsiveContainer width="100%" height="100%">
           <LineChart data={dados} margin={{ top: 20, right: 20, left: 40, bottom: 20 }}>
@@ -49,6 +64,14 @@ const Grafico: React.FC = () => {
               dataKey="pibTotal"
               name="PIB Total (Mil Dólares)"
               stroke="#8884d8"
+              strokeWidth={2}
+              dot={false}
+            />
+            <Line
+              type="monotone"
+              dataKey="pibPerCapita"
+              name="PIB per Capita (Dólares)"
+              stroke="#82ca9d"
               strokeWidth={2}
               dot={false}
             />
