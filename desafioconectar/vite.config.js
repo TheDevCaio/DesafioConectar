a55a46464a41ec09940e84e99e26ad8ffbf607a4
Caio Dev <caioinnocencio@gmail.com>
2025-05-02 05:24:00 -0300

    Atualizando dependencias de rotina.

--- desafioconectar/vite.config.js
+++ desafioconectar/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
-    exclude: [...configDefaults.exclude],
+    exclude: [...configDefaults.exclude, 'dist'],
   },
-})
+});
